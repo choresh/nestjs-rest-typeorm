bfe2a35485cba680d6a0e505e8011640b63df84d
choresh <raanan34@gmail.com>
2024-06-12 15:09:15 +0300

    Phase 7.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,47 +8,89 @@ import {
   Delete,
   UseInterceptors,
   UploadedFile,
+  Header,
+  StreamableFile,
+  Query,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { User } from './entities/user.entity';
-import { DeleteResult, UpdateResult } from 'typeorm';
+import { DeleteResult, FindOptionsWhere, UpdateResult } from 'typeorm';
+import { Readable } from 'stream';
+
+function strToBoolean(str: string): boolean {
+  if (str === 'true') {
+    return true;
+  }
+  if (str === 'false') {
+    return false;
+  }
+  throw new Error(`Invalid value ('${str}' is not 'true' or 'false')`)
+}
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto): Promise<User> {
-    return this.usersService.create(createUserDto);
+  async createOne(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return await this.usersService.createOne(createUserDto);
   }
 
   @Get()
-  findAll(): Promise<User[]> {
-    return this.usersService.findAll();
+  async findAll(): Promise<User[]> {
+    return await this.usersService.findAll();
+  }
+
+  @Get('some')
+  async findSome(
+    @Query('firstName') firstName: string,
+    @Query('lastName') lastName: string,
+    @Query('isActive') isActive: string
+  ): Promise<User[]> {
+    const where: FindOptionsWhere<User> = {
+      firstName,
+      lastName,
+      isActive: strToBoolean(isActive),
+    };
+    return await this.usersService.findSome(where);
+  }
+
+  @Get('export')
+  @Header('Content-Type', 'application/json')
+  @Header('Content-Disposition', 'attachment; filename="users.json"')
+  async export() {
+    const data = await this.usersService.findAll();
+    const dataStream = new Readable();
+    dataStream.push(JSON.stringify(data));
+    dataStream.push(null); // End the stream
+    return new StreamableFile(dataStream);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string): Promise<User | null> {
-    return this.usersService.findOne(+id);
+  async findOneById(@Param('id') id: string): Promise<User | null> {
+    return await this.usersService.findOneById(Number(id));
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto): Promise<UpdateResult> {
-    return this.usersService.update(+id, updateUserDto);
+  async updateOneById(
+    @Param('id') id: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ): Promise<UpdateResult> {
+    return await this.usersService.updateOneById(Number(id), updateUserDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string): Promise<DeleteResult> {
-    return this.usersService.remove(+id);
+  async removeOneById(@Param('id') id: string): Promise<DeleteResult> {
+    return await this.usersService.removeOneById(Number(id));
   }
 
   @Post('import')
   @UseInterceptors(FileInterceptor('file'))
-  import(@UploadedFile() file: Express.Multer.File): Promise<User[]> {
+  async import(@UploadedFile() file: Express.Multer.File): Promise<User[]> {
     const createUserDtos: CreateUserDto[] = JSON.parse(file.buffer?.toString());
-    return this.usersService.createMany(createUserDtos);
+    return await this.usersService.createMany(createUserDtos);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { DeleteResult, Repository, UpdateResult } from 'typeorm';
+import {
+  DeleteResult,
+  FindOptionsWhere,
+  Repository,
+  UpdateResult,
+} from 'typeorm';
 import { User } from './entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 
@@ -12,7 +17,7 @@ export class UsersService {
     private usersRepository: Repository<User>,
   ) {}
 
-  async create(createUserDto: CreateUserDto): Promise<User> {
+  async createOne(createUserDto: CreateUserDto): Promise<User> {
     const newEntity = this.usersRepository.create(createUserDto);
     return await this.usersRepository.save(newEntity);
   }
@@ -21,23 +26,31 @@ export class UsersService {
     return this.usersRepository.find();
   }
 
-  findOne(id: number): Promise<User | null> {
+  async findSome(where: FindOptionsWhere<User>): Promise<User[]> {
+    return this.usersRepository.find({
+      where,
+    });
+  }
+
+  findOneById(id: number): Promise<User | null> {
     return this.usersRepository.findOneBy({ id });
   }
 
-  async update(
+  async updateOneById(
     id: number,
     updateUserDto: UpdateUserDto,
   ): Promise<UpdateResult> {
     return await this.usersRepository.update({ id }, updateUserDto);
   }
 
-  async remove(id: number): Promise<DeleteResult> {
+  async removeOneById(id: number): Promise<DeleteResult> {
     return await this.usersRepository.delete(id);
   }
 
   async createMany(createUserDtos: CreateUserDto[]): Promise<User[]> {
-    const newEntities = createUserDtos.map(currCreateUserDto => this.usersRepository.create(currCreateUserDto));
+    const newEntities = createUserDtos.map((currCreateUserDto) =>
+      this.usersRepository.create(currCreateUserDto),
+    );
     return await this.usersRepository.save(newEntities);
   }
 }
